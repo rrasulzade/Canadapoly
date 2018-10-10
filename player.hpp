efd6659abbd5ceff7410ad5428fbb3ea07be7b65
Rasul <rrasulza@yahoo.com>
2018-10-09 21:43:55 -0400

    Player header initial commit

--- player.hpp
+++ player.hpp
@@ -9,6 +9,33 @@
 #ifndef player_hpp
 #define player_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Player {
+    string name;
+    const char piece;
+    int money;
+    int position;
+    std:vector<Ownable* > buildings;
+
+    static int player_num;
+
+public:
+    Player();
+    ~Player();
+
+    bool hasMonopoly();
+    void buy();
+    void auction();
+    void mortgage();
+    void unmortgage();
+    void trade();
+    void bankrupt();
+    void improve();
+
+};
 
 #endif /* player_hpp */
